8b5aaa585abe961985695c794bb0801a15b36dfd
AnushaYadaV4 <anu468mjl@gmail.com>
2022-11-24 18:34:08 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+
 import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
+
 function App() {
-  return (
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      locationOfExpenditure:'Delhi'
+    },
+    {
+    id: 'e2', 
+    title: 'New TV', 
+    amount: 799.49, 
+    date: new Date(2021, 2, 12), 
+    locationOfExpenditure:'Mumbai'
+     },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      locationOfExpenditure:'Hyderabad'
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      locationOfExpenditure:'Banglore'
+    }
+  ];
+
+return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -11,8 +45,20 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h2>Let's Get Started</h2>
-        <ExpenseItem></ExpenseItem>
-        <a
+
+        <div>
+      {expenses.map( expense=> (
+        <li>
+          <ExpenseItem title={expense.title}
+                   amount={expense.amount}
+                   date={expense.date}
+                  locationOfExpenditure={expense.locationOfExpenditure}
+          ></ExpenseItem>
+        </li>
+      ))}
+    </div>
+
+      <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,19 @@
-function ExpenseItem(){
+import './ExpenseItem.css';
+
+
+function ExpenseItem(props){
+    
     return (
-        <div>
-            <div>Food Rs 10</div>
-            <div>Petrol Rs 100</div>
-            <div>Movies Rs 200</div>
+        <div className='expense-item'>
+                <div>{props.date.toISOString()}</div>
+        
+                <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
+                <div className='expense-item__description'>{props.locationOfExpenditure}</div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default ExpenseItem;
